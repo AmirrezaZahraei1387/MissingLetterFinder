adb62f12c56f1bf2bfd6ddaf6fca06a371dfa575
Amirreza <131318547+AmirrezaZahraei1387@users.noreply.github.com>
2023-08-16 23:34:45 +0000

    the python file trainer is added with all the imports needed

---
+++ datatr/trainer.py
@@ -0,0 +1,14 @@
+
+# standard library
+import os
+import csv
+
+
+# third party libraries
+from nltk.tokenize import word_tokenize
+from nltk import bigrams, pos_tag
+from numpy import array
+from numpy import unique
+
+
+def getAllPathes()
